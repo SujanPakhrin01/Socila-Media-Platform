00b79bcf58a866cdbbe8d00f45a1f3c348717d88
sujan-py <sujanmakalu@gmail.com>
2026-01-02 14:04:02 +0545

    view update

--- App/views.py
+++ App/views.py
@@ -2,7 +2,83 @@ from django.shortcuts import render
 from .models import *
 from .serializers import UserSerializer, PostSerializer, CommentSerializer, LikeSerializer, FollowSerializer, AnalyticsSerializer, NotificationSerializer
 from rest_framework.views import APIView
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+
+class Home(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        posts = self.get_queryset()
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data)
+    
+class CommentView(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        posts = self.get_queryset()
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data)
+    
+class FollowView(ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        posts = self.get_queryset()
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data)
+    
+class NotifictionView(ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        posts = self.get_queryset()
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data)
+    
+class AnalyticsView(ModelViewSet):
+    queryset = Analytics.objects.all()
+    serializer_class = AnalyticsSerializer
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        posts = self.get_queryset()
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data)
+    
+class Home(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        posts = self.get_queryset()
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data)
+    
+class LikeView(ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        posts = self.get_queryset()
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data)
+    
+
 
 
 
